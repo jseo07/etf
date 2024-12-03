a37011648f5ad6b45f2b152c1ee21b3391c7c3fe
jseo079 <jangwonseo079@gmail.com>
2024-12-03 01:06:55 -0800

    Added data cleaning methods, and data plots

--- etf/etfapp/api_func.py
+++ etf/etfapp/api_func.py
@@ -1,4 +1,7 @@
 import requests
+import json
+import plotly.express as px
+import pandas as pd
 
 ##############################################
 # API FUNCTIONALITY
@@ -31,3 +34,44 @@ def clean_search_data(data):
         extracted_data.append({'symbol': symbol, 'name': name})
     
     return extracted_data
+
+def clean_monthly_data(data_dict):
+    keys = data_dict.keys()
+    result={"date":"", "dividend":0, "adjusted_close":0, "dividend_rate":0}
+    results = []
+    for key in keys:
+        data = data_dict[key]
+        if float(data["7. dividend amount"]) != 0:
+            temp = result.copy()
+            temp["date"] = key
+            temp["dividend"] = float(data["7. dividend amount"])
+            temp["adjusted_close"] = float(data["5. adjusted close"])
+            temp["dividend_rate"] = float(data["7. dividend amount"])/float(data["5. adjusted close"])
+            results.append(temp)
+    return results
+
+with open('C:/Users/tjwkd/Documents/etf/etf/etfapp/temp.json') as file:
+        monthly_data = json.load(file)
+        monthly_data = monthly_data['Monthly Adjusted Time Series']
+        cleaned_monthly_data = clean_monthly_data(monthly_data)
+
+def data_to_df(data_dict):
+    df_dict = {
+        "date": [],
+        "price": [],
+        "dividend": [],
+        "dividend_rate": []
+        }
+    dates = []
+    dividends = []
+    price = []
+    dividend_rates = []
+    for data in cleaned_monthly_data:
+        df_dict["date"].append(pd.to_datetime((data["date"])))
+        df_dict["price"].append(data["adjusted_close"])
+        df_dict["dividend"].append(data["dividend"])
+        df_dict["dividend_rate"].append(data["dividend_rate"])
+
+    df = pd.DataFrame(df_dict)
+    return df
+
--- etf/etfapp/views.py
+++ etf/etfapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, JsonResponse
 import requests
 from etfapp.api_func import *
 import json
+from plotly.io import to_html
+
+
 # Create your views here.
 
 def index(request):
@@ -10,37 +13,62 @@ def index(request):
 
 def result(request, symbol):
     symbol = extract_symbol(symbol)
+
+    #### Uncomment to test with real api
     #monthly_data = request_data("TIME_SERIES_MONTHLY_ADJUSTED", symbol)
-    with open('/Users/jangwonseo/Documents/Documents/Courses/etf/etf/etfapp/temp.json') as file:
+    #monthly_data = monthly_data['Monthly Adjusted Time Series']
+    #cleaned_monthly_data = clean_monthly_data(monthly_data)
+
+    # Loads Temporary file
+    with open('C:/Users/tjwkd/Documents/etf/etf/etfapp/temp.json') as file:
         monthly_data = json.load(file)
         monthly_data = monthly_data['Monthly Adjusted Time Series']
+        cleaned_monthly_data = clean_monthly_data(monthly_data)
 
-    context = {'symbol':symbol, 'monthly_data':monthly_data}
+    df = data_to_df(cleaned_monthly_data)
+    fig_price = px.line(df, x="date", y="price", title="Date vs Price", labels={"Date": "Date", "Price": "Price"})
+    fig_dividend = px.line(df, x="date", y="dividend", title="Date vs Dividend", labels={"Date": "Date", "Dividend": "Dividend"})
+    fig_dividend_rate = px.line(df, x="date", y="dividend_rate", title="Date vs Dividend Rate", labels={"Date": "Date", "Dividend Rate": "Dividend Rate"})
+
+    fig_price_html = to_html(fig_price, full_html=False)
+    fig_dividend_html = to_html(fig_dividend, full_html=False)
+    fig_dividend_rate_html = to_html(fig_dividend_rate, full_html=False)
+
+    context = {'symbol':symbol, 
+               'price_plot':fig_price_html, 
+               'dividend_plot':fig_dividend_html,
+               'dividend_rate_plot':fig_dividend_rate_html}
     return render(request, 'result.html', context)
 
 def live_search(request):
     if request.method == 'GET':
         search_query = request.GET.get('query', '')  # Get the query string from the request
-        print(search_query)
         if search_query:
             #data = search_auto(search_query)
             ###########
             # temporarily put sample data because of api daily limit
             ##########
             #"""
+            
             data = [
-                        {'symbol': 'JEPI', 'name': 'JPMorgan Equity Premium Income ETF'},
-                        {'symbol': 'JEPQ', 'name': 'JPMorgan Nasdaq Equity Premium Income ETF'},
-                        {'symbol': 'JEP.FRK', 'name': 'SalMar ASA'},
-                        {'symbol': 'JEPAX', 'name': 'JPMORGAN EQUITY PREMIUM INCOME FUND CLASS A'},
-                        {'symbol': 'JEPCX', 'name': 'JPMORGAN EQUITY PREMIUM INCOME FUND CLASS C'},
-                        {'symbol': 'JEPIX', 'name': 'JPMORGAN EQUITY PREMIUM INCOME FUND CLASS I'},
-                        {'symbol': 'JEPMX', 'name': 'JPMORGAN U.S. RESEARCH EQUITY PLUS FUND CLASS R6'},
-                        {'symbol': 'JEPRX', 'name': 'JPMORGAN EQUITY PREMIUM INCOME FUND CLASS R6'},
-                        {'symbol': 'JEPSX', 'name': 'JPMORGAN EQUITY PREMIUM INCOME FUND CLASS R5'},
-                        {'symbol': 'JEPG.LON', 'name': 'Global Equity Premium Income UCITS ETF'}
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'},
+                        {'symbol': 'IBM', 'name': 'International Business Machines Corporation'}
                     ]
             #"""
+
             
             # Filter the mock data based on the query
             filtered_data = [item for item in data if search_query.lower() in item['name'].lower() or search_query.upper() in item['symbol']]
@@ -48,7 +76,6 @@ def live_search(request):
             filtered_data = []
 
         # Return a JsonResponse with the filtered data
-        print(filtered_data)
         return JsonResponse({'results': filtered_data})
 
     return JsonResponse({'error': 'Invalid request method'}, status=400)
@@ -61,3 +88,4 @@ def extract_symbol(str):
     str = str[17:]
     str = str[:-4]
     return str
+    
