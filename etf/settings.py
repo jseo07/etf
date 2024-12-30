a4ec492b6869e4ddbee59cfdf6320abc24564cfd
jseo079 <jangwonseo079@gmail.com>
2024-12-30 13:48:49 -0800

    changed secret-key

--- etf/settings.py
+++ etf/settings.py
@@ -29,7 +29,8 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-15df_p4mzvd-)ub=uzjy95^#$c)u8slkmd&)&!bs1k$1#%7@1+'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
